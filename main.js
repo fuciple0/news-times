3c461913e04cdb6152348547e199f8ce2d82f057
Jung_min Choi <fusion_up@naver.com>
2024-07-09 14:11:56 +0900

    try-catch add

--- main.js
+++ main.js
@@ -1,53 +1,61 @@
-const API_Key = `671fb72d7d804b99b1926a0d123a54e1`
-let newsList = []
-const menus = document.querySelectorAll(".menus button")
-menus.forEach(menu => menu.addEventListener("click",(event)=>getNewsByCategory(event)))
+const API_Key = `671fb72d7d804b99b1926a0d123a54e1`;
+let newsList = [];
+const menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
 
-const getLatestNews = async() =>{
-    // const url = new URL(
-    //     `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_Key}`
-    // )
-    const url = new URL(
-        `https://graceful-sherbet-defe7a.netlify.app/top-headlines`
-    )
-    const response = await fetch(url)
-    const data = await response.json()
-    newsList = data.articles
-    render()
-    console.log("ddd", newsList)
-}
+let url = new URL(`https://graceful-sherbet-defe7a.netlify.app/top-headlines`);
 
-const getNewsByCategory = async(event) =>{
-    const category = event.target.textContent.toLowerCase()
+const getNews = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (response.status === 200) {
+      if (data.articles.length === 0) {
+        throw new Error("No result for this search");
+      }
+      newsList = data.articles;
+      render();
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    errorRender(error.message);
+  }
+};
 
-    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_Key}`
-    // )
-    const url = new URL(`https://graceful-sherbet-defe7a.netlify.app/top-headlines?category=${category}`
-    )
+const getLatestNews = async () => {
+  // const url = new URL(
+  //     `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_Key}`
+  // )
+  url = new URL(`https://graceful-sherbet-defe7a.netlify.app/top-headlines`);
+  getNews();
+};
 
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log("ddd", data)
-    newsList = data.articles
-    render()
-    
-}
-const getNewsByKeyword = async()=>{
-    const keyword = document.getElementById("search-input").value
-    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_Key}`
-    // )
-    const url = new URL(`https://graceful-sherbet-defe7a.netlify.app/top-headlines?q=${keyword}`
-    )
-
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log("keyword", data)
-    newsList = data.articles
-    render()
-}
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_Key}`
+  // )
+  url = new URL(
+    `https://graceful-sherbet-defe7a.netlify.app/top-headlines?category=${category}`
+  );
+  getNews();
+};
+const getNewsByKeyword = async () => {
+  const keyword = document.getElementById("search-input").value;
+  // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_Key}`
+  // )
+  url = new URL(
+    `https://graceful-sherbet-defe7a.netlify.app/top-headlines?q=${keyword}`
+  );
+  getNews();
+};
 
 const render = () => {
-    const newsHTML = newsList.map(news=>`
+  const newsHTML = newsList
+    .map(
+      (news) => `
         <div class="row news">
                 <div class="col-lg-4">
                     <img class="news-img-size" src=${news.urlToImage} alt="">
@@ -57,14 +65,22 @@ const render = () => {
                     <p>${news.description}</p>
                     <div>${news.source.name} * ${news.publishedAt}</div>
                 </div>
-            </div>`).join('')
+            </div>`
+    )
+    .join("");
 
-    document.getElementById("news-board").innerHTML = newsHTML
-}
+  document.getElementById("news-board").innerHTML = newsHTML;
+};
 
-getLatestNews()
+const errorRender = (errorMessage) => {
+  const errorHTML = `<div class="alert alert-danger" role="alert">
+    ${errorMessage}
+    </div>`;
 
+  document.getElementById("news-board").innerHTML = errorHTML;
+};
 
+getLatestNews();
 
 //1.버튼에 클릭이벤트 주기
 //2.카테고리별 뉴스 가져오기
