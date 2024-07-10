19505315e9ba0e805a3423242ba6635e08852fa9
Jung_min Choi <fusion_up@naver.com>
2024-07-10 18:50:29 +0900

    style edit

--- main.js
+++ main.js
@@ -56,6 +56,16 @@ const getNewsByCategory = async (event) => {
   );
   getNews();
 };
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+};
+
 const getNewsByKeyword = async () => {
   const keyword = document.getElementById("search-input").value;
 
@@ -69,19 +79,20 @@ const getNewsByKeyword = async () => {
 };
 
 const render = () => {
-  const newsHTML = newsList
-    .map(
-      (news) => `
-        <div class="row news">
-                <div class="col-lg-4">
-                    <img class="news-img-size" src=${news.urlToImage} alt="">
-                </div>
-                <div class="col-lg-8">
-                    <h2>${news.title}</h2>
-                    <p>${news.description}</p>
-                    <div>${news.source.name} * ${news.publishedAt}</div>
-                </div>
-            </div>`
+  const newsHTML = newsList.map((news) => `
+   <div class="row news">
+     <div class="col-lg-4">
+       <img class="news-img-size" src=${news.urlToImage} onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU'; this.onerror=null;" ></div>
+       <div class="col-lg-8">
+       <a class="title" target="_blank" href="${news.url}">${news.title}</a>
+       <p>${news.description == null || news.description == ""
+          ? "내용없음"
+          : news.description.length > 200
+          ? news.description.substring(0, 200) + "..."
+          : news.description}</p>
+        <div>${news.source.name || "no source"}  ${moment(news.publishedAt).fromNow()}</div>
+      </div>
+    </div>`
     )
     .join("");
 
@@ -110,23 +121,23 @@ const paginationRender = () => {
     lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
 
   if (firstPage >= 6) {
-    paginationHTML = `<li class="page-item" onclick="moveToPage(1)"><a class="page-link" href="#">&lt;&lt;</a></li>
+    paginationHTML = `<li class="page-item" onclick="moveToPage(1)"><a class="page-link" href='#js-bottom'>&lt;&lt;</a></li>
   <li class="page-item" onclick="moveToPage(${
     page - 1
-  })"><a class="page-link" href="#">&lt;</a></li>`;
+  })"><a class="page-link" href='#js-bottom'>&lt;</a></li>`;
   }
 
   for (let i = firstPage; i <= lastPage; i++) {
     paginationHTML += `<li class="page-item ${
       i === page ? "active" : ""
-    }" onclick="moveToPage(${i})"><a class="page-link" >${i}</a></li>`;
+    }" onclick="moveToPage(${i})"><a class="page-link" href='#js-bottom' >${i}</a></li>`;
   }
 
   if (lastPage < totalPages) {
     paginationHTML += `<li class="page-item" onclick="moveToPage(${
       page + 1
-    })"><a class="page-link" href="#">&gt;</a></li>
-  <li class="page-item" onclick="moveToPage(${totalPages})"><a class="page-link" href="#">&gt;&gt;</a></li>`;
+    })"><a class="page-link" href='#js-program-detail-bottom'>&gt;</a></li>
+  <li class="page-item" onclick="moveToPage(${totalPages})"><a class="page-link" href='#js-bottom'>&gt;&gt;</a></li>`;
   }
 
   document.querySelector(".pagination").innerHTML = paginationHTML;
@@ -136,6 +147,14 @@ const moveToPage = (pageNum) => {
   console.log("🚀 ~ moveToPage ~ moveToPage:", pageNum);
   page = pageNum;
   getNews();
+};
+
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
 };
 
 getLatestNews();
