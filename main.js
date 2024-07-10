07a64906b8942140b1c5f4e88816617225c22812
Jung_min Choi <fusion_up@naver.com>
2024-07-10 10:13:03 +0900

    pagination add

--- main.js
+++ main.js
@@ -6,9 +6,16 @@ menus.forEach((menu) =>
 );
 
 let url = new URL(`https://graceful-sherbet-defe7a.netlify.app/top-headlines`);
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
 
 const getNews = async () => {
   try {
+    url.searchParams.set("page", page); // => &page=page
+    url.searchParams.set("pageSize", pageSize);
+
     const response = await fetch(url);
     const data = await response.json();
     if (response.status === 200) {
@@ -16,7 +23,9 @@ const getNews = async () => {
         throw new Error("No result for this search");
       }
       newsList = data.articles;
+      totalResults = data.totalResults;
       render();
+      paginationRender();
     } else {
       throw new Error(data.message);
     }
@@ -78,6 +87,47 @@ const errorRender = (errorMessage) => {
     </div>`;
 
   document.getElementById("news-board").innerHTML = errorHTML;
+};
+
+const paginationRender = () => {
+  const totalPages = Math.ceil(totalResults / pageSize);
+  const pageGroup = Math.ceil(page / groupSize);
+  const lastPage = pageGroup * groupSize;
+  //마지막 페이지 그룹이 그룹사이즈보다 작다?
+  if (lastPage > totalPages) {
+    lastPage = totalPages;
+  }
+  const firstPage =
+    lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+
+  let paginationHTML = `<li class="page-item" onclick="moveToPage(${
+    page - 1
+  })"><a class="page-link" href="#">Previous</a></li>`;
+  for (let i = firstPage; i <= lastPage; i++) {
+    paginationHTML += `<li class="page-item ${
+      i === page ? "active" : ""
+    }" onclick="moveToPage(${i})"><a class="page-link" >${i}</a></li>`;
+  }
+
+  paginationHTML += `<li class="page-item" onclick="moveToPage(${
+    page + 1
+  })"><a class="page-link" href="#">Next</a></li>`;
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+
+  //   <nav aria-label="Page navigation example">
+  //   <ul class="pagination">
+  //     <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+  //     <li class="page-item"><a class="page-link" href="#">1</a></li>
+  //     <li class="page-item"><a class="page-link" href="#">2</a></li>
+  //     <li class="page-item"><a class="page-link" href="#">3</a></li>
+  //     <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  //   </ul>
+  // </nav>
+};
+const moveToPage = (pageNum) => {
+  console.log("🚀 ~ moveToPage ~ moveToPage:", pageNum);
+  page = pageNum;
+  getNews();
 };
 
 getLatestNews();
